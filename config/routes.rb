50c8992e49868d65ff7a0032a4e68472bc9845b7
tararico <emipu.uq@gmail.com>
2019-02-22 22:04:48 +0900

    add destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'posts/index'
   get 'posts/:id' => "posts#show"
   get 'posts/:id/edit' => 'posts#edit'
+  post 'posts/:id/update' => 'posts#update'
+  post 'posts/:id/destroy' => 'posts#destroy'
   get '/' => 'home#top'
   get '/about' => 'home#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
