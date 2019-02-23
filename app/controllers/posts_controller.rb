a1202e65f313467d32ce80f9b648eea11de8ea47
tararico <emipu.uq@gmail.com>
2019-02-23 17:32:35 +0900

    add edit error_messages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,8 +28,12 @@ class PostsController < ApplicationController
   def update
     @post = Post.find_by(id: params[:id])
     @post.content = params[:content]
-    @post.save
-    redirect_to("/posts/index")
+    if @post.save
+      flash[:notice] = "投稿を編集しました"
+      redirect_to("/posts/index")
+    else
+      render("/posts/edit")
+    end
   end
 
   def destroy
